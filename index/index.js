60940a4568e4f3f63cb8b9dc506f0617e3aa8b37
Robert <osipovitchr@gmail.com>
2022-03-13 10:59:46 -0400

    create hamburger menu

--- index/index.js
+++ index/index.js
@@ -18,13 +18,28 @@ setTimeout(() => {
     $('.flag').fadeIn(800)
 }, 1500)
 
-// setTimeout(() => {
-//     $('.three').fadeIn(800)
-// }, 1500)
 
-// setTimeout(() => {
-//     $('.one').fadeIn(800)
-// }, 1000)
+
+// This is for my Navigation hamburger menu ///
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger= document.querySelector(".hamburger");
+const closeIcon= document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
 
 
 //// IMG Carousel /////
