63e7265115ec4894f8e8ecf932871d0a996ba3a3
Robert <osipovitchr@gmail.com>
2022-03-15 15:55:47 -0400

    add jQuery to upper-assembly hero

--- 3-upper-assemblies/upper.js
+++ 3-upper-assemblies/upper.js
@@ -1,1 +1,13 @@
-// soon to come
+
+// jQuery for Hero Section 
+
+$('a.company-name').hide();
+$('p.hero-text').hide();
+
+setTimeout(() => {
+    $('a.company-name').fadeIn();
+}, 500)
+
+setTimeout(() => {
+    $('p.hero-text').fadeIn();
+}, 1150)
